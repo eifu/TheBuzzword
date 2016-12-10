cbeb9e3e83247d9ca86aa4fd1de93d499b0f54cb
eifu <etomita18@gmail.com>
2016-12-10 11:55:32 -0500

    remove try-catch in trieworddata by clearing up the json data

--- Buzzword/src/data/TrieWordData.java
+++ Buzzword/src/data/TrieWordData.java
@@ -5,31 +5,29 @@ public class TrieWordData {
 
     Node root = new Node();
 
-    public void addWord(String w){
+    public void addWord(String w) {
         Node node = root;
-        try {
-            for (char c : w.toCharArray()) {
-                if (node.children[c - 'A'] == null) {
-                    node.children[c - 'A'] = new Node();
-                }
-                node = node.children[c - 'A'];
+
+        for (char c : w.toCharArray()) {
+            if (node.children[c - 'A'] == null) {
+                node.children[c - 'A'] = new Node();
             }
-        }catch (IndexOutOfBoundsException e){
-            e.printStackTrace();
+            node = node.children[c - 'A'];
         }
+
         node.item = true;
     }
 
-    public  boolean findWord(String w){
+    public boolean findWord(String w) {
         Node node = root;
-        for(char c :w.toCharArray()){
-            if (node.children[c-'A']==null){
+        for (char c : w.toCharArray()) {
+            if (node.children[c - 'A'] == null) {
                 return false;
             }
-            node = node.children[c-'A'];
+            node = node.children[c - 'A'];
         }
 
-        if (node.item){
+        if (node.item) {
             return true;
         }
 
@@ -37,7 +35,7 @@ public class TrieWordData {
     }
 
 
-    class Node{
+    class Node {
         boolean item;
         Node[] children = new Node[26];
     }
